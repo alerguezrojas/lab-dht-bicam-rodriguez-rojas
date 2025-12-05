bc06daf1f66f005f050a39316a980a769b3bb3dd
Alejandro Rodríguez Rojas <alu0101317038@ull.edu.es>
2025-12-05 21:14:40 +0000

    Fase 3: Dockerfile

---
+++ src/main/java/BiCIAMApp.java
@@ -0,0 +1,6 @@
+public class BiCIAMApp {
+    public static void main(String[] args) {
+        System.out.println("BiCIAM - Biblioteca de Algoritmos Metaheurísticos");
+        System.out.println("Application started successfully.");
+    }
+}
